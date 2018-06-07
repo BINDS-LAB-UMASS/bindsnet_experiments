ec10e9ebd82b944e1d474aba97edd6d27c5f2fe9
Dan Saunders <djsaunde@umass.edu>
2018-06-07 08:09:13 -0400

    Changing grid search

--- bash/mnist/grid_search_diehl_and_cook_2015.sh
+++ bash/mnist/grid_search_diehl_and_cook_2015.sh
@@ -1,22 +1,22 @@
 for seed in 0
 do
-	for n_neurons in 100 400 
+	for n_neurons in 400
 	do 
 		for n_train in 30000
 		do
 			for n_test in 10000
 			do
-				for excite in 20 25 30 35 40 45 50 60 70 80 90 100
+				for excite in {20..50..10}
 				do
-					for inhib in 20 25 30 35 40 45 50 60 70 80 90 100
+					for inhib in {20..50..10}
 					do
 						for time in 350
 						do
-							for theta_plus in 0.05 0.1 0.25 0.5 1.0
+							for theta_plus in 0.05 0.25 1.0
 							do
-								for theta_decay in 1e-7 1e-6 1e-5 1e-4
+								for theta_decay in 1e-7 1e-6 1e-5
 								do
-									for intensity in 0.25 0.5 0.75 1.0
+									for intensity in 0.25 0.5
 									do
 										sbatch submit_diehl_and_cook_2015.sh $seed $n_neurons $n_train $n_test \
 											   $excite $inhib $time $theta_plus $theta_decay $intensity
