181c80096187267eb893d637d4c64a5457c231ad
Dan Saunders <djsaunde@umass.edu>
2018-06-19 18:25:40 -0400

    Fixing incorrect file saving logic.

--- scripts/mnist/diehl_and_cook_2015.py
+++ scripts/mnist/diehl_and_cook_2015.py
@@ -296,10 +296,16 @@ else:
 
 if not os.path.isfile(os.path.join(path, name)):
     with open(os.path.join(path, name), 'w') as f:
-        f.write('random_seed,n_neurons,n_train,n_test,excite,' + \
-                'inhib,time,timestep,intensity,progress_interval,' + \
-                'update_interval,mean_all_activity,mean_proportion weighting,' +
-                'max_all_activity,max_proportion weighting\n')
+        if train:
+            f.write('random_seed,n_neurons,n_train,excite,' + \
+                    'inhib,time,timestep,intensity,progress_interval,' + \
+                    'update_interval,mean_all_activity,mean_proportion weighting,' +
+                    'max_all_activity,max_proportion weighting\n')
+        else:
+            f.write('random_seed,n_neurons,n_train,n_test,excite,' + \
+                    'inhib,time,timestep,intensity,progress_interval,' + \
+                    'update_interval,mean_all_activity,mean_proportion weighting,' +
+                    'max_all_activity,max_proportion weighting\n')
 
 with open(os.path.join(path, name), 'a') as f:
     f.write(','.join(to_write) + '\n')
