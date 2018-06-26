13bc46698b1505fc66c1d59bf40cffd18e77fdb8
Dan Saunders <djsaunde@umass.edu>
2018-06-26 13:34:23 -0400

    Adding utility functions and scripts.

--- analysis/best_hyperparams.py
+++ analysis/best_hyperparams.py
@@ -4,6 +4,7 @@ import pandas as pd
 
 from utils import sort_best_params
 
+
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('--model', type=str, default='diehl_and_cook_2015')
@@ -11,6 +12,7 @@ if __name__ == '__main__':
     parser.add_argument('--train', dest='train', action='store_true')
     parser.add_argument('--test', dest='train', action='store_false')
     parser.add_argument('--fix', default=[], nargs='*')
+    parser.add_argument('--vary', default=[], nargs='*')
     parser.add_argument('--metric', type=str, default='mean_all_activity')
     parser.add_argument('--top', type=int, default=None)
     parser.set_defaults(train=True)
@@ -19,6 +21,7 @@ if __name__ == '__main__':
     model = args.model
     data = args.data
     train = args.train
+    vary = args.vary
     metric = args.metric
     top = args.top
 
@@ -28,7 +31,7 @@ if __name__ == '__main__':
     for i in range(int(len(args.fix) / 2)):
         fix[args.fix[2 * i]] = args.fix[2 * i + 1]
 
-    df = sort_best_params(model, data, train, fix, metric, top)
+    df = sort_best_params(model, data, train, fix, vary, metric, top)
 
     with pd.option_context('display.max_rows', None, 'display.max_columns', None):
         print(df)
--- analysis/utils.py
+++ analysis/utils.py
@@ -5,20 +5,23 @@ import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
 
+from itertools import product
+
 sns.set()
+
 
 def sort_best_params(model='diehl_and_cook_2015',
 				     data='mnist',
 				     train=True,
                      fix=None,
+                     vary=None,
 				     metric='mean_all_activity',
                      top=None):
-    '''
+    """
     Gets the train or test results from a particular data / model combo's
     experiments, fix certain parameters to values, and return the results sorted
     in descending order of some metric.
-    '''
-
+    """
     name = '_'.join([model, data])
     f = 'train.csv' if train else 'test.csv'
     path = os.path.join('..', 'results', name, f)
@@ -31,20 +34,49 @@ def sort_best_params(model='diehl_and_cook_2015',
     for v in fix:
         df = df[df[v] == fix[v]]
 
-    df = df.sort_values(by=metric, ascending=False)
+    unique = {}
+    for v in vary:
+        unique[v] = df[v].unique()
 
-    if top is None:
-        return df
+    df.drop(labels=[c for c in df.columns if 'max' in c],
+            axis='columns',
+            inplace=True)
 
-    return df.head(top)
+    groupby_columns = list(set(df.columns) - {'random_seed'} - \
+                           set([c for c in df.columns if 'mean' in c]))
+
+    average_columns = [c for c in df.columns if 'mean' in c]
+    for c in average_columns:
+        df[c] = df[c].astype(float)
+
+    dfs = {}
+    for p in product(*unique.values()):
+        temp = df.copy()
+        for i, v in enumerate(vary):
+            temp = temp[temp[v] == p[i]]
+        dfs[p] = temp
+
+    averaged = {}
+    for key, df in dfs.items():
+        df = df.groupby(by=groupby_columns)[average_columns].agg({'mean' : np.mean, 'std' : np.std})
+        df = df.sort_values(by=('mean', metric), ascending=False)
+
+        if df.size != 0:
+            if top is None:
+                averaged[key] = df
+            else:
+                averaged[key] = df.head(1)
+
+    return averaged
+
 
 def compute_stats(df,
                   stats=['mean', 'min', 'max', 'std'],
                   metrics=['mean_all_activity', 'mean_proportion_weighting']):
-    '''
+    """
     Computes stats over metrics for a particular results dataframe, over all
     parameters besides random seeds.
-    '''
+    """
     for s in stats:
         for m in metrics:
             comp = df.apply(s)
