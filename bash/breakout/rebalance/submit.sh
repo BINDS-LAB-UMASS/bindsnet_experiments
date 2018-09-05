0210af797cb059989e1a60898a3ff9a4648d0371
djsaunde <djsaunde@umass.edu>
2018-09-05 11:40:48 -0400

    Adding cropped + rebalanced data script and accompanying bash scripts.

---
+++ bash/breakout/rebalance/submit.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+#
+#SBATCH --partition=longq
+#SBATCH --time=02-00:00:00
+#SBATCH --mem=8000
+#SBATCH --account=rkozma
+#SBATCH --output=../../output/rebalance_%j.out
+#SBATCH --cpus-per-task=8
+
+seed=${1:-0}
+n_neurons=${2:-100}
+n_train=${3:-5500}
+n_test=${4:-1300}
+inhib=${5:-25.0}
+time=${6:-350}
+theta_plus=${7:-0.05}
+theta_decay=${8:-1e-7}
+
+cd ../../../scripts/breakout/
+source activate py36
+
+echo $seed $n_neurons $n_train $n_test $inhib $time $theta_plus $theta_decay
+
+python rebalance.py --train --seed $seed --n_neurons $n_neurons --n_train $n_train \
+		            --n_test $n_test --inhib $inhib --time $time \
+			        --theta_plus $theta_plus --theta_decay $theta_decay
+python rebalance.py --test --seed $seed --n_neurons $n_neurons --n_train $n_train \
+                    --n_test $n_test --inhib $inhib --time $time \
+			        --theta_plus $theta_plus --theta_decay $theta_decay
+exit
--- scripts/misc/can.py
+++ scripts/misc/can.py
@@ -1,81 +1,90 @@
-import matplotlib.pyplot as plt
 import torch
+import argparse
+import matplotlib.pyplot as plt
+
 from bindsnet.network import *
+from bindsnet.analysis.plotting import plot_spikes
 
 
 plt.ioff()
 
-# param
-Nu_of_CANs = 5
-input_size = 1600
-Nu_Neurons_in_CAN = 1000
-Excitation_percent_in_CAN = 1
-running_time = 5000
+parser = argparse.ArgumentParser()
+parser.add_argument('--n_cans', type=int, default=5)
+parser.add_argument('--input_size', type=int, default=5)
+parser.add_argument('--n_neurons', type=int, default=1000)
+parser.add_argument('--excitation', type=float, default=0.0)
+parser.add_argument('--time', type=int, default=500)
+parser.add_argument('--plot', dest='plot', action='store_true')
+parser.set_defaults(plot=False)
+args = parser.parse_args()
+
+n_cans = args.n_cans
+input_size = args.input_size
+n_neurons = args.n_neurons
+excitation = args.excitation
+time = args.time
+plot = args.plot
 
 network = Network(dt=1.0)  # Instantiates network.
 In = nodes.Input(n=input_size)
 network.add_layer(layer=In, name='X')
-Mi = monitors.Monitor(obj=In, state_vars=['s'], time=running_time)
+Mi = monitors.Monitor(obj=In, state_vars=['s'], time=time)
+network.add_monitor(monitor=Mi, name='input monitor')
 M = []
 
 
-for i in range(Nu_of_CANs):
+for i in range(n_cans):
     # add CAN column
-    CAN = nodes.IzhikevichNodes(n=Nu_Neurons_in_CAN, excitatory=Excitation_percent_in_CAN)
+    # CAN = nodes.IzhikevichNodes(n=n_neurons, excitatory=excitation)
+    CAN = nodes.LIFNodes(n=n_neurons)
 
     # create random weights for internal connection of CAN
-    w = (torch.rand(Nu_Neurons_in_CAN, Nu_Neurons_in_CAN))
-    # make the weights of inhibitory neurons negative
-    temp = CAN.excitatory == False
-
-    if torch.sum(temp) > 0:
-        w = torch.where(temp, 0.5 * w, -w)
+    n_ex = int(excitation * 10000)
+    w = torch.rand(n_neurons, n_neurons)
+    w[:n_ex] *= 0.5
+    w[n_ex:] *= -1
 
     # create weights between input to output.
-    wi = torch.zeros(input_size, Nu_Neurons_in_CAN)
-    wi[i, :] = 1.
+    wi = torch.zeros(input_size, n_neurons)
+    wi[i] = 1
 
     # connect the internal connection in CAN
     C = topology.Connection(source=CAN, target=CAN, w=w)
     # connect the CAN to the input layer
     Ci = topology.Connection(source=In, target=CAN, w=wi)
 
-    M.append(monitors.Monitor(obj=CAN, state_vars=['s'], time=running_time))
+    M.append(monitors.Monitor(obj=CAN, state_vars=['s', 'v'], time=time))
 
     temp_CAN_name = 'CAN' + str(i)
     network.add_layer(layer=CAN, name=temp_CAN_name)
-    network.add_monitor(monitor=Mi, name='input monitor')
     network.add_monitor(monitor=M[i], name=temp_CAN_name)
-    network.add_connection(connection=C, source=temp_CAN_name, target= temp_CAN_name)
+    network.add_connection(connection=C, source=temp_CAN_name, target=temp_CAN_name)
     network.add_connection(connection=Ci, source='Input Layer', target=temp_CAN_name)
 
 
 # Create Poisson-distributed spike train inputs.
 #data = 15 * torch.rand(1, input_size)  # Generate random Poisson rates for 100 input neurons.
 #train = encoding.poisson(datum=data, time=5000)  # Encode input as 5000ms Poisson spike trains.
-train = 3.5 * torch.rand(running_time, input_size)
+train = torch.bernoulli(torch.rand(time, input_size))
 
 # # Simulate network on generated spike trains.
-network.run(inpts={'X': train}, time=running_time)  # Run network simulation.
+network.run(inpts={'X': train}, time=time)  # Run network simulation.
 
-# Plot spikes of input and output layers.
-temp_spikes = torch.zeros(Nu_Neurons_in_CAN * Nu_of_CANs, running_time)
-for i in range(Nu_of_CANs):
-    temp = i*Nu_Neurons_in_CAN
-    temp_spikes[temp:temp + Nu_Neurons_in_CAN, :] = M[i].get('s')
+if plot:
+    # Plot spikes of input and output layers.
+    temp_spikes = torch.zeros(n_neurons * n_cans, time)
+    voltages = torch.zeros(n_neurons * n_cans, time)
+    for i in range(n_cans):
+        temp = i * n_neurons
+        temp_spikes[temp:temp + n_neurons, :] = M[i].get('s')
+        voltages[temp:temp + n_neurons, :] = M[i].get('v')
 
-spikes = {'X': Mi.get('s'), 'Y': temp_spikes}
+    voltages = voltages.numpy()
+    spikes = {'X': Mi.get('s'), 'Y': temp_spikes}
 
-fig, axes = plt.subplots(2, 1, figsize=(12, 7))
-for i, layer in enumerate(spikes):
-    axes[i].matshow(spikes[layer], cmap='binary')
-    axes[i].set_title('%s spikes' % layer)
-    axes[i].set_xlabel('Time');
-    axes[i].set_ylabel('Index of neuron')
-    axes[i].set_xticks(());
-    axes[i].set_yticks(())
-    axes[i].set_aspect('auto')
-
-plt.tight_layout()
-plt.show()
+    plot_spikes(spikes)
+    # figure = plt.figure()
+    # plt.plot(voltages.T[:, ::100])
+    plt.tight_layout()
+    plt.show()
 
