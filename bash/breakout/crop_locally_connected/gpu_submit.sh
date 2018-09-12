e2eab5255535b4a8a1aed217af496360fa038369
djsaunde <djsaunde@umass.edu>
2018-09-12 16:57:51 -0400

    Fixing input parsing bugs in bash scripts.

--- bash/breakout/crop_locally_connected/gpu_submit.sh
+++ bash/breakout/crop_locally_connected/gpu_submit.sh
@@ -9,26 +9,28 @@
 #SBATCH --cpus-per-task=8
 
 seed=${1:-0}
-kernel_size=${2:-'25 36'}
-stride=${3:-'5 6'}
-n_filters=${4:-25}
-n_train=${5:-16000}
-n_test=${6:-10000}
-inhib=${7:-25.0}
-time=${8:-350}
-theta_plus=${9:-0.05}
-theta_decay=${10:-1e-7}
-norm=${11:-0.2}
+k1=${2:-25}
+k2=${3:-36}
+s1=${4:-5}
+s2=${5:-6}
+n_filters=${6:-25}
+n_train=${7:-16000}
+n_test=${8:-10000}
+inhib=${9:-25.0}
+time=${10:-350}
+theta_plus=${11:-0.05}
+theta_decay=${12:-1e-7}
+norm=${13:-0.2}
 
 cd ../../../scripts/breakout/
 source activate py36
 
-echo $seed $kernel_size $stride $n_filters $n_train $n_test $inhib $time $theta_plus $theta_decay $norm
+echo $seed $k1 $k2 $s1 $s2 $n_filters $n_train $n_test $inhib $time $theta_plus $theta_decay $norm
 
-python crop_locally_connected.py --train --seed $seed --kernel_size $kernel_size --stride $stride \
+python crop_locally_connected.py --train --seed $seed --kernel_size $k1 $k2 --stride $s1 $s2 \
                                  --n_filters $n_filters --n_train $n_train --n_test $n_test --inhib $inhib \
                                  --time $time --theta_plus $theta_plus --theta_decay $theta_decay --norm $norm
-python crop_locally_connected.py --test --seed $seed --kernel_size $kernel_size --stride $stride \
+python crop_locally_connected.py --test --seed $seed --kernel_size $k1 $k2 --stride $s1 $s2 \
                                  --n_filters $n_filters --n_train $n_train --n_test $n_test --inhib $inhib \
                                  --time $time --theta_plus $theta_plus --theta_decay $theta_decay --norm $norm
 exit
