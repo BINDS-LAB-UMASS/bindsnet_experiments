cfaf7f90e6442c4ecae9444c0edd0d1c12db1963
djsaunde <djsaunde@umass.edu>
2018-09-20 12:35:24 -0400

    Modifying grid search params.

--- bash/mnist/crop_locally_connected/grid_search.sh
+++ bash/mnist/crop_locally_connected/grid_search.sh
@@ -6,9 +6,9 @@ do
     do
         for stride in 2 4
         do
-            for n_filters in 9 16 25 36 49
+            for n_filters in 16 25 36 49 64 81 100
             do
-                for crop in 2 3 4 5
+                for crop in 2 4
                 do
                     for n_train in 60000
                     do
@@ -24,7 +24,7 @@ do
                                         do
                                             for intensity in 0.5
                                             do
-                                                for norm in 0.1 0.15 0.2 0.25
+                                                for norm in 0.1 0.15 0.2
                                                 do
                                                     sbatch submit.sh $seed $kernel_size $stride $n_filters $crop \
                                                                      $n_train $n_test $inhib $time $theta_plus \
