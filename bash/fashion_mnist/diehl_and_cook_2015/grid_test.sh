c599bd95d5eef30cdf975fd19342d42e54c97d66
Dan Saunders <djsaunde@umass.edu>
2018-10-09 12:26:27 -0400

    Adding bash scripts for D&C 2015 on Fashion MNIST.

---
+++ bash/fashion_mnist/diehl_and_cook_2015/grid_test.sh
@@ -0,0 +1,35 @@
+#!/usr/bin/env bash
+
+for seed in 0
+do
+    for n_neurons in 100 200 300 400 500
+    do
+        for n_train in 60000
+        do
+            for n_test in 10000
+            do
+                for inhib in 100 250 500
+                do
+                    for time in 100 200 300 400 500
+                    do
+                        for theta_plus in 0.05
+                        do
+                            for theta_decay in 1e-7
+                            do
+                                for intensity in 0.5
+                                do
+                                    for norm in 50 75 100 125 150
+                                    do
+                                        sbatch test.sh $seed $n_neurons $n_train $n_test \
+                                                       $inhib $time $theta_plus $theta_decay \
+                                                       $intensity $norm
+                                    done
+                                done
+                            done
+                        done
+                    done
+                done
+            done
+        done
+    done
+done
---
+++ bash/fashion_mnist/diehl_and_cook_2015/test.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+#
+#SBATCH --partition=longq
+#SBATCH --time=03-00:00:00
+#SBATCH --mem=12000
+#SBATCH --account=rkozma
+#SBATCH --output=../../output/diehl_and_cook_2015_fashion_mnist_%j.out
+#SBATCH --cpus-per-task=8
+
+seed=${1:-0}
+n_neurons=${2:-100}
+n_train=${3:-60000}
+n_test=${4:-10000}
+inhib=${5:-25.0}
+time=${6:-350}
+theta_plus=${7:-0.05}
+theta_decay=${8:-1e-7}
+intensity=${9:-0.5}
+norm=${10:-100}
+
+cd ../../../scripts/fashion_mnist/
+source activate py36
+
+echo $seed $n_neurons $n_train $n_test $inhib $time $theta_plus $theta_decay $intensity norm
+
+python diehl_and_cook_2015.py --test --seed $seed --n_neurons $n_neurons --n_train $n_train \
+							  --n_test $n_test --inhib $inhib --time $time \
+							  --theta_plus $theta_plus --theta_decay $theta_decay \
+							  --intensity $intensity --norm norm
+exit
