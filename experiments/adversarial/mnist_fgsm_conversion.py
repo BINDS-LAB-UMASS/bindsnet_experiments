35ba32e117950b08c9f55ac0ca2e6ba84e5fffb7
Dan Saunders <djsaunde@umass.edu>
2018-11-01 15:28:12 -0400

    More work on conversion experiments.

--- experiments/adversarial/mnist_fgsm_conversion.py
+++ experiments/adversarial/mnist_fgsm_conversion.py
@@ -1,5 +1,7 @@
 import os
 import argparse
+import sys
+
 import numpy as np
 import matplotlib.pyplot as plt
 
@@ -9,48 +11,52 @@ import torch
 import torch.nn as nn
 import torch.optim as optim
 
-from bindsnet.datasets import CIFAR10
+import tensorflow as tf
+
+from bindsnet.datasets import MNIST
 from bindsnet.conversion import ann_to_snn
 from bindsnet.network.monitors import Monitor
 from bindsnet.analysis.plotting import plot_spikes
 
-params_path = os.path.join('..', '..', 'params', 'cifar10_fully_conv')
+from cleverhans.model import CallableModelWrapper
+from cleverhans.attacks import FastGradientMethod
+from cleverhans.utils_pytorch import convert_pytorch_model_to_tf
+
+data = 'mnist'
+model = 'fgsm'
+
+params_path = os.path.join('..', '..', 'params', data, model)
 if not os.path.isdir(params_path):
     os.makedirs(params_path)
 
 if torch.cuda.is_available():
     torch.set_default_tensor_type('torch.cuda.FloatTensor')
 
-
-class Flatten(nn.Module):
-    def forward(self, x):
-        return x.view(x.size()[0], -1)
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
 
 
-class LeNet(nn.Module):
-
+class FullyConnectedNetwork(nn.Module):
+    # language=rst
+    """
+    Simply fully-connected network implemented in PyTorch.
+    """
     def __init__(self):
-        super(LeNet, self).__init__()
+        super(FullyConnectedNetwork, self).__init__()
 
-        self.f = nn.Sequential(
-            nn.Conv2d(in_channels=3, out_channels=6, kernel_size=5),
-            nn.ReLU(inplace=True),
-            nn.Conv2d(in_channels=6, out_channels=16, kernel_size=5),
-            nn.ReLU(inplace=True),
-            Flatten(),
-            nn.Linear(in_features=9216, out_features=120),
-            nn.ReLU(inplace=True),
-            nn.Linear(in_features=120, out_features=84),
-            nn.ReLU(inplace=True),
-            nn.Linear(in_features=84, out_features=10)
-        )
+        self.fc1 = nn.Linear(784, 256)
+        self.relu1 = nn.ReLU()
+        self.fc2 = nn.Linear(256, 128)
+        self.relu2 = nn.ReLU()
+        self.fc3 = nn.Linear(128, 10)
 
     def forward(self, x):
-        return self.f(x)
+        x = self.relu1(self.fc1(x))
+        x = self.relu2(self.fc2(x))
+        x = self.fc3(x)
+        return x
 
 
-def main(seed=0, n_epochs=1, batch_size=100, time=50, update_interval=50,
-         n_examples=1000, percentile=100, plot=False, save=True):
+def main(seed=0, n_epochs=5, batch_size=100, time=50, update_interval=50, plot=False, save=True):
 
     np.random.seed(seed)
 
@@ -61,17 +67,18 @@ def main(seed=0, n_epochs=1, batch_size=100, time=50, update_interval=50,
         torch.manual_seed(seed)
 
     print()
-    print('Loading CIFAR-10 data...')
+    print('Loading MNIST data...')
+    print()
 
     # Get the CIFAR-10 data.
-    images, labels = CIFAR10('../../data/CIFAR10', download=True).get_train()
+    images, labels = MNIST('../../data/MNIST', download=True).get_train()
     images /= images.max()  # Standardizing to [0, 1].
-    images = images.permute(0, 3, 1, 2)
+    images = images.view(-1, 784)
     labels = labels.long()
 
-    test_images, test_labels = CIFAR10('../../data/CIFAR10', download=True).get_test()
+    test_images, test_labels = MNIST('../../data/MNIST', download=True).get_test()
     test_images /= test_images.max()  # Standardizing to [0, 1].
-    test_images = test_images.permute(0, 3, 1, 2)
+    test_images = test_images.view(-1, 784)
     test_labels = test_labels.long()
 
     if torch.cuda.is_available():
@@ -80,11 +87,11 @@ def main(seed=0, n_epochs=1, batch_size=100, time=50, update_interval=50,
         test_images = test_images.cuda()
         test_labels = test_labels.cuda()
 
-    model_name = '_'.join([
-        str(x) for x in [seed, n_epochs, batch_size, time, update_interval, n_examples]
-    ])
+    ANN = FullyConnectedNetwork()
 
-    ANN = LeNet()
+    model_name = '_'.join([
+        str(x) for x in [seed, n_epochs, batch_size, time, update_interval]
+    ])
 
     # Specify loss function.
     criterion = nn.CrossEntropyLoss()
@@ -129,34 +136,66 @@ def main(seed=0, n_epochs=1, batch_size=100, time=50, update_interval=50,
                 accuracies.append(correct.item() * 100)
 
             outputs = ANN.forward(test_images)
-            test_loss = criterion(outputs, test_labels).item()
+            loss = criterion(outputs, test_labels).item()
             predictions = torch.max(outputs, 1)[1]
             test_accuracy = ((test_labels == predictions).sum().float() / test_labels.numel()).item() * 100
 
-            train_loss = np.mean(losses)
-            train_acc = np.mean(accuracies)
+            avg_loss = np.mean(losses)
+            avg_acc = np.mean(accuracies)
 
-            print(f'Epoch: {i+1} / {n_epochs}; Train Loss: {train_loss:.4f}; Train Accuracy: {train_acc:.4f}')
-            print(f'\tTest Loss: {test_loss:.4f}; Test Accuracy: {test_accuracy:.4f}')
+            print(f'Epoch: {i+1} / {n_epochs}; Train Loss: {avg_loss:.4f}; Train Accuracy: {avg_acc:.4f}')
+            print(f'\tTest Loss: {loss:.4f}; Test Accuracy: {test_accuracy:.4f}')
 
         if save:
             torch.save(ANN.state_dict(), os.path.join(params_path, model_name + '.pt'))
 
-    outputs = ANN.forward(images)
-    loss = criterion(outputs, labels)
+    outputs = ANN.forward(test_images)
+    loss = criterion(outputs, test_labels)
     predictions = torch.max(outputs, 1)[1]
-    accuracy = ((labels == predictions).sum().float() / labels.numel()).item() * 100
+    accuracy = ((test_labels == predictions).sum().float() / test_labels.numel()).item() * 100
 
     print()
-    print(f'(Post training) Training Loss: {loss:.4f}; Training Accuracy: {accuracy:.4f}')
+    print(f'(Post training) Test Loss: {loss:.4f}; Test Accuracy: {accuracy:.4f}')
+
+    print()
+    print('Evaluating ANN on adversarial examples from FSGM method...')
+
+    # Convert pytorch model to a tf_model and wrap it in cleverhans.
+    tf_model_fn = convert_pytorch_model_to_tf(ANN)
+    cleverhans_model = CallableModelWrapper(tf_model_fn, output_layer='logits')
+
+    sess = tf.Session()
+    x_op = tf.placeholder(tf.float32, shape=(None, 784,))
+
+    # Create an FGSM attack.
+    fgsm_op = FastGradientMethod(cleverhans_model, sess=sess)
+    fgsm_params = {
+        'eps': 0.2, 'clip_min': 0.0, 'clip_max': 1.0
+    }
+    adv_x_op = fgsm_op.generate(x_op, **fgsm_params)
+    adv_preds_op = tf_model_fn(adv_x_op)
+
+    # Run an evaluation of our model against FGSM white-box attack.
+    total = 0
+    correct = 0
+    adv_preds = sess.run(adv_preds_op, feed_dict={x_op: test_images})
+    correct += (np.argmax(adv_preds, axis=1) == test_labels).sum()
+    total += len(test_images)
+    accuracy = float(correct) / total
+
+    print()
+    print('Adversarial accuracy: {:.3f}'.format(accuracy * 100))
 
     print()
     print('Converting ANN to SNN...')
 
+    with sess.as_default():
+        test_images = adv_x_op.eval(feed_dict={x_op: test_images})
+
+    test_images = torch.tensor(test_images)
+
     # Do ANN to SNN conversion.
-    SNN = ann_to_snn(
-        ANN, input_shape=(1, 3, 32, 32), data=images[:n_examples], percentile=percentile
-    )
+    SNN = ann_to_snn(ANN, input_shape=(784,), data=test_images, percentile=100)
 
     for l in SNN.layers:
         if l != 'Input':
@@ -164,50 +203,46 @@ def main(seed=0, n_epochs=1, batch_size=100, time=50, update_interval=50,
                 Monitor(SNN.layers[l], state_vars=['s', 'v'], time=time), name=l
             )
 
+    print()
+    print('Testing SNN on FGSM-modified MNIST data...')
+    print()
+
+    # Test SNN on MNIST data.
     spike_ims = None
     spike_axes = None
     correct = []
 
-    print()
-    print('Testing SNN on MNIST data...')
-    print()
+    n_images = test_images.size(0)
 
-    # Test SNN on MNIST data.
     start = t()
-    for i in range(images.size(0)):
+    for i in range(n_images):
         if i > 0 and i % update_interval == 0:
-            print(
-                f'Progress: {i} / {images.size(0)}; Elapsed: {t() - start:.4f}; Accuracy: {np.mean(correct) * 100:.4f}')
+            accuracy = np.mean(correct) * 100
+            print(f'Progress: {i} / {n_images}; Elapsed: {t() - start:.4f}; Accuracy: {accuracy:.4f}')
             start = t()
 
-        inpts = {'Input': images[i].repeat(time, 1, 1, 1, 1)}
-        SNN.run(inpts=inpts, time=time)
+        SNN.run(inpts={'Input': test_images[i].repeat(time, 1, 1)}, time=time)
 
         spikes = {layer: SNN.monitors[layer].get('s') for layer in SNN.monitors}
         voltages = {layer: SNN.monitors[layer].get('v') for layer in SNN.monitors}
-        prediction = torch.softmax(voltages['9'].sum(1), 0).argmax()
-        correct.append((prediction == labels[i]).item())
+        prediction = torch.softmax(voltages['fc3'].sum(1), 0).argmax()
+        correct.append((prediction == test_labels[i]).item())
 
         SNN.reset_()
 
         if plot:
-            inpts = {'Input': inpts['Input'].view(time, -1).t()}
-            spikes = {**inpts, **spikes}
-            spike_ims, spike_axes = plot_spikes(
-                {k: spikes[k].cpu() for k in spikes}, ims=spike_ims, axes=spike_axes
-            )
+            spikes = {k: spikes[k].cpu() for k in spikes}
+            spike_ims, spike_axes = plot_spikes(spikes, ims=spike_ims, axes=spike_axes)
             plt.pause(1e-3)
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('--seed', type=int, default=0)
-    parser.add_argument('--n_epochs', type=int, default=3)
+    parser.add_argument('--n_epochs', type=int, default=5)
     parser.add_argument('--batch_size', type=int, default=100)
     parser.add_argument('--time', type=int, default=50)
     parser.add_argument('--update_interval', type=int, default=50)
-    parser.add_argument('--n_examples', type=int, default=1000)
-    parser.add_argument('--percentile', type=float, default=99)
     parser.add_argument('--plot', dest='plot', action='store_true')
     parser.add_argument('--no-save', dest='save', action='store_false')
     parser.set_defaults(plot=False, save=True)
