d78edc2062a1cb067c8545bf4bf276afb0631732
Dan Saunders <djsaunde@umass.edu>
2018-11-05 13:19:43 -0500

    Updating bash scripts.

--- bash/mnist/conv/grid_search.sh
+++ bash/mnist/conv/grid_search.sh
@@ -15,13 +15,13 @@ do
 			do
 				for stride in 2
 				do
-					for n_filters in 25 50 100 150
+					for n_filters in 50 100 150
 					do
-                        for time in 25 50 100
+                        for time in 50 100
                         do
                             for lr in 0.01 0.005 0.001
                             do
-                                for lr_decay in 1.0 0.99 0.975 0.95
+                                for lr_decay in 1.0 0.99 0.975
                                 do
                                     sbatch submit.sh $seed $n_train $n_test $kernel_size $stride $n_filters \
                                                      $padding $inhib $time $dt $intensity
--- bash/mnist/real_dac/grid_search.sh
+++ bash/mnist/real_dac/grid_search.sh
@@ -12,11 +12,11 @@ do
         do
             for n_test in 10000
             do
-                for time in 25 50 100 250
+                for time in 50 100 250
                 do
                     for lr in 0.1 0.05 0.01 0.005
                     do
-                        for lr_decay in 1.0 0.99 0.95
+                        for lr_decay in 1.0 0.99 0.975 0.95
                         do
                             sbatch submit.sh $seed $n_neurons $n_train $n_test $inhib $time \
                                              $lr $lr_decay $theta_plus $theta_decay
