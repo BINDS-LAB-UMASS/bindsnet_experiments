bf9bbb80561e9ccf6132e92c8543f11a3926a44f
Dan Saunders <djsaunde@umass.edu>
2018-12-11 11:16:19 -0500

    Fixing typo.

--- bash/robustness/mnist/dac_synapses/grid_search.sh
+++ bash/robustness/mnist/dac_synapses/grid_search.sh
@@ -2,8 +2,8 @@
 
 for seed in 0 1 2 3 4
 do
-    for p_delete in 0 0.1 0.2 0.3 0.4 0.5. 0.6 0.7 0.8 0.9 1
+    for p_destroy in 0 0.1 0.2 0.3 0.4 0.5. 0.6 0.7 0.8 0.9 1
     do
-        sbatch submit.sh $seed $p_delete
+        sbatch submit.sh $seed $p_destroy
     done
 done
--- bash/robustness/mnist/dac_synapses/submit.sh
+++ bash/robustness/mnist/dac_synapses/submit.sh
@@ -8,9 +8,9 @@
 #SBATCH --cpus-per-task=8
 
 seed=${1:-0}
-p_delete=${2:-0.1}
+p_destroy=${2:-0.1}
 
 cd ../../../../experiments/robustness/mnist/
-echo $seed $p_delete
-python dac_synapses.py --seed $seed --p_delete $p_delete
+echo $seed $p_destroy
+python dac_synapses.py --seed $seed --p_destroy $p_destroy
 exit
