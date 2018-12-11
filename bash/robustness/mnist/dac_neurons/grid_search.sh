58f2ec158db4ba852cf6a002155eab5ba2153892
Dan Saunders <djsaunde@umass.edu>
2018-12-11 11:18:48 -0500

    Fixing another typo.

--- bash/robustness/mnist/dac_neurons/grid_search.sh
+++ bash/robustness/mnist/dac_neurons/grid_search.sh
@@ -2,7 +2,7 @@
 
 for seed in 0 1 2 3 4
 do
-    for p_remove in 0 0.1 0.2 0.3 0.4 0.5. 0.6 0.7 0.8 0.9 1
+    for p_remove in 0 0.1 0.2 0.3 0.4 0.5 0.6 0.7 0.8 0.9 1
     do
         sbatch submit.sh $seed $p_remove
     done
