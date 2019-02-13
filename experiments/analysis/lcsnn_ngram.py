d00055bb157d510e41270e7c23d7e22451aeb387
Dan Saunders <djsaunde@umass.edu>
2019-02-13 11:16:32 -0500

    Small change.

--- experiments/analysis/lcsnn_ngram.py
+++ experiments/analysis/lcsnn_ngram.py
@@ -2,81 +2,10 @@ import os
 import torch
 
 from tqdm import tqdm
-from itertools import product
-from typing import Dict, Tuple
 from experiments import ROOT_DIR
+from bindsnet.evaluation import ngram, update_ngram_scores
 
 location = 'gpu' if torch.cuda.is_available() else 'cpu'
-
-
-def ngram(spikes: torch.Tensor, ngram_scores: Dict[Tuple[int, ...], torch.Tensor], n_labels: int,
-          n: int) -> torch.Tensor:
-    # language=rst
-    """
-    Predicts between ``n_labels`` using ``ngram_scores``.
-
-    :param spikes: Spikes of shape ``(n_examples, time, n_neurons)``.
-    :param ngram_scores: Previously recorded scores to update.
-    :param n_labels: The number of target labels in the data.
-    :param n: The max size of n-gram to use.
-    :return: Predictions per example.
-    """
-    predictions = []
-    for activity in spikes:
-        score = torch.zeros(n_labels)
-
-        # Aggregate all of the firing neurons' indices
-        fire_order = []
-        for t in range(activity.size()[0]):
-            ordering = torch.nonzero(activity[t].view(-1))
-            if ordering.numel() > 0:
-                fire_order += ordering[:, 0].tolist()
-
-        # Consider all n-gram sequences.
-        for j in range(len(fire_order) - n):
-            if tuple(fire_order[j:j + n]) in ngram_scores:
-                score += ngram_scores[tuple(fire_order[j:j + n])]
-
-        predictions.append(torch.argmax(score))
-
-    return torch.Tensor(predictions).long()
-
-
-def update_ngram_scores(spikes: torch.Tensor, labels: torch.Tensor, n_labels: int, n: int,
-                        ngram_scores: Dict[Tuple[int, ...], torch.Tensor]) -> Dict[Tuple[int, ...], torch.Tensor]:
-    # language=rst
-    """
-    Updates ngram scores by adding the count of each spike sequence of length n from the past ``n_examples``.
-
-    :param spikes: Spikes of shape ``(n_examples, time, n_neurons)``.
-    :param labels: The ground truth labels of shape ``(n_examples)``.
-    :param n_labels: The number of target labels in the data.
-    :param n: The max size of n-gram to use.
-    :param ngram_scores: Previously recorded scores to update.
-    :return: Dictionary mapping n-grams to vectors of per-class spike counts.
-    """
-    for i, activity in enumerate(spikes):
-        # Obtain firing order for spiking activity.
-        fire_order = []
-
-        # Aggregate all of the firing neurons' indices.
-        for t in range(spikes.size(1)):
-            # Gets the indices of the neurons which fired on this timestep.
-            ordering = torch.nonzero(activity[t]).view(-1)
-            if ordering.numel() > 0:  # If there was more than one spike...
-                # Add the indices of spiked neurons to the fire ordering.
-                ordering = ordering.tolist()
-                fire_order.append(ordering)
-
-        # Check every sequence of length n.
-        for order in zip(*(fire_order[k:] for k in range(n))):
-            for sequence in product(*order):
-                if sequence not in ngram_scores:
-                    ngram_scores[sequence] = torch.zeros(n_labels)
-
-                ngram_scores[sequence][int(labels[i])] += 1
-
-    return ngram_scores
 
 
 def main():
