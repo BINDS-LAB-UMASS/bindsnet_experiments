595f399a5a3b8f1129bbf68b1993dc78118c1c74
Daniel Saunders <djsaunde@umass.edu>
2019-05-01 11:34:22 -0400

    Update diehl_and_cook_2015.py

--- experiments/mnist/diehl_and_cook_2015.py
+++ experiments/mnist/diehl_and_cook_2015.py
@@ -36,7 +36,7 @@ for path in [params_path, spikes_path, curves_path, results_path, confusion_path
 
 
 def main(seed=0, n_neurons=100, n_train=60000, n_test=10000, inhib=100, lr=1e-2, lr_decay=1, time=350, dt=1,
-         theta_plus=0.05, theta_decay=1e-7, intensity=1, progress_interval=10, update_interval=250, plot=False,
+         theta_plus=0.05, theta_decay=1e7, intensity=1, progress_interval=10, update_interval=250, plot=False,
          train=True, gpu=False):
 
     assert n_train % update_interval == 0 and n_test % update_interval == 0, \
@@ -329,7 +329,7 @@ if __name__ == '__main__':
     parser.add_argument('--time', default=350, type=int, help='simulation time')
     parser.add_argument('--dt', type=float, default=1, help='simulation integreation timestep')
     parser.add_argument('--theta_plus', type=float, default=0.05, help='adaptive threshold increase post-spike')
-    parser.add_argument('--theta_decay', type=float, default=1e-7, help='adaptive threshold decay time constant')
+    parser.add_argument('--theta_decay', type=float, default=1e7, help='adaptive threshold decay time constant')
     parser.add_argument('--intensity', type=float, default=0.5, help='constant to multiple input data by')
     parser.add_argument('--progress_interval', type=int, default=10, help='interval to print train, test progress')
     parser.add_argument('--update_interval', default=250, type=int, help='no. examples between evaluation')
